972a8de7698a7926828a1880a3cb628c6c1a5935
Eric <eric@posoroko.com>
2021-09-24 19:11:24 +0200

    update

--- main.js
+++ main.js
@@ -1,6 +1,28 @@
 const topTitle_h1 =  document.getElementById('topTitle');
 const topTitleBox_div = document.getElementById('topTitleBox');
 const moto_p = document.querySelector('.moto');
+const menuText_p = document.getElementById('menuText');
+const menuPanel_div = document.getElementById('menuPanel');
+const bigLogoBox_div = document.getElementById('bigLogoBox');
+
+
+
+function hauteurRedimentionnable(minSize,cible, parent, largeur, direction){
+
+    let = fontSizeCount = minSize;
+
+function rechercheHauteur(cible, parent, largeur) {
+        console.log('hauteur');
+        cible.style.fontSize = fontSizeCount + 'px';
+        if(cible.offsetHeight < parent.offsetHeight * largeur) {
+            fontSizeCount ++;
+            rechercheHauteur(cible, parent, largeur);
+        }else {
+            fontSizeCount = minSize;
+        }
+    }
+    rechercheHauteur(cible, parent, largeur);
+}
 
 
 
@@ -9,26 +31,28 @@ function largeurRedimentionnable(minSize,cible, parent, largeur){
     let = fontSizeCount = minSize;
 
 function rechercheLargeur(cible, parent, largeur) {
+    console.log('largeur');
         cible.style.fontSize = fontSizeCount + 'px';
-        if(cible.offsetHeight < parent.offsetHeight * largeur)
-        {
+        if(cible.offsetWidth < parent.offsetWidth * largeur) {
             fontSizeCount ++;
             rechercheLargeur(cible, parent, largeur);
-        }
-        else
-        {
+        }else {
             fontSizeCount = minSize;
         }
     }
     rechercheLargeur(cible, parent, largeur);
-
 }
+
 
 //filtre les actions 'click' sur l'object window
 function handleClick(e) {
     if(e.target.classList.contains('trad')) {
         traduire(e.target);
     }
+}
+function handleScroll() {
+    let gap = window.scrollY * 0.5;
+    bigLogoBox_div.style.marginTop = gap + 'px';
 }
 
 //traduit n'importequelle cible ayant la classs 'trad'
@@ -41,7 +65,8 @@ function traduire(targ) {
             {filter: 'blur(0px)'}
         ], {
             duration: 600,
-            fill: 'forwards'
+            fill: 'forwards',
+            easing: 'cubic-bezier(0,.87,1,.24)'
         }
     )
     setTimeout(() => {
@@ -56,12 +81,15 @@ function traduire(targ) {
 }
 
 
+
+/* EVENT LISTENERS */
 window.addEventListener('resize', function(){
     largeurRedimentionnable(20, topTitle_h1, topTitleBox_div, 1);
 });
 window.addEventListener('click', handleClick);
-largeurRedimentionnable(20, topTitle_h1, topTitleBox_div, 1);
+window.addEventListener('scroll', handleScroll);
 
+hauteurRedimentionnable(20, topTitle_h1, topTitleBox_div, 1);
 
 
 
@@ -71,13 +99,16 @@ const texts = {
         fr: `Définir vos besoin, votre image et vos objectifs.<br>
             Créer le site web qui représente votre réalité.<br>
             Construire des outils ergonomiques qui  <br>
-            servent les besoins des utilisateurs.`,
+            servent vos utilisateurs.`,
         eng: `Define your needs, your brand and your objectives. <br>
             Create the website that represents your reality. <br>
-            Build ergonomic tools that serve the user's needs.`
+            Build ergonomic tools that serve your users.`
     },
     subtitle: {
         fr: `Studio de <br> développement web <br> et de design`,
         eng: `Web development <br> and design <br> studio`
     }
 }
+
+moto_p.innerHTML = texts.moto.fr;
+subtitle.innerHTML = texts.subtitle.fr;
